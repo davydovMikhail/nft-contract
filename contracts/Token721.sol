a2c4595abe14b84e11515d5c49d8b6a2ab0c6c35
davydovMikhail <davydov27977@gmail.com>
2022-03-12 18:32:55 +0700

    tests

--- contracts/Token721.sol
+++ contracts/Token721.sol
@@ -1,14 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-// import "node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
-// import "node_modules/@openzeppelin/contracts/access/Ownable.sol";
-// import "node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-
-// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol";
-// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
